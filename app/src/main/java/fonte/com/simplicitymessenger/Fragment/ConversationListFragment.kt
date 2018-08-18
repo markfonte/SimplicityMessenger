7dd9b53a6684b74e4528218868fcd01ad9835d88
Mark Fonte <mark@fonte.com>
2018-08-18 11:12:14 -0400

    loads full inbox list to conversation list screen. Need to implement lazy loading asap

--- app/src/main/java/fonte/com/simplicitymessenger/Fragment/ConversationListFragment.kt
+++ app/src/main/java/fonte/com/simplicitymessenger/Fragment/ConversationListFragment.kt
@@ -2,71 +2,74 @@ package fonte.com.simplicitymessenger.Fragment
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.os.Bundle
-import android.provider.Telephony
-import android.support.v4.app.Fragment
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import fonte.com.simplicitymessenger.R
 import android.content.ContentResolver
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.net.Uri
-import android.support.design.widget.Snackbar
+import android.os.Bundle
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
-import android.telephony.SmsManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.conversation_list_fragment.*
-import java.util.jar.Manifest
+import android.widget.ListView
+import fonte.com.simplicitymessenger.R
 
 
 class ConversationListFragment : Fragment() {
-    private var smsList: ArrayList<String>? = null
+    private var conversationListView: ListView? = null
+
     @SuppressLint("InflateParams")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        return inflater.inflate(R.layout.conversation_list_fragment, null)
+        val rootView: View =  inflater.inflate(R.layout.conversation_list_fragment, null)
+        conversationListView = rootView.findViewById(R.id.conversation_list)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val permissionCheck: Int = ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.READ_SMS)
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            loadConversationList()
+        } else {
+            ActivityCompat.requestPermissions(activity as Activity, arrayOf(android.Manifest.permission.READ_SMS), 100)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val permissionCheck : Int = ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.READ_SMS )
-        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
-            loadConversationList()
-        }
-        else {
-            ActivityCompat.requestPermissions(activity as Activity, arrayOf(android.Manifest.permission.READ_SMS), 100)
-        }
     }
 
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if(requestCode == 100) {
-            loadConversationList()
-        }
-        else {
-            Log.e(LOG_TAG, "Cannot be displayed without required permissions")
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadConversationList()
+            } else {
+                Log.e(LOG_TAG, "Cannot be displayed without required permissions")
+            }
         }
     }
 
     private fun loadConversationList() {
-        val inboxUri : Uri = Uri.parse("content://sms/inbox")
-        smsList = ArrayList(0)
-        val contentResolver : ContentResolver? = activity?.contentResolver
+        val inboxUri: Uri = Uri.parse("content://sms/inbox")
+        val listItems : ArrayList<String?> = ArrayList(0)
+        val contentResolver: ContentResolver? = activity?.contentResolver
         val cursor: Cursor? = contentResolver?.query(inboxUri, null, null, null, null)
-        while(cursor?.moveToNext()!!) {
+        while (cursor?.moveToNext()!! ) {
             val number: String = cursor.getString(cursor.getColumnIndexOrThrow("address"))
-
             val body: String = cursor.getString(cursor.getColumnIndexOrThrow("body"))
-            smsList!!.add("Number: $number \nBody: $body")
+            listItems.add("Number: $number \nBody: $body")
         }
         cursor.close()
-        val adapter: ArrayAdapter<String> = ArrayAdapter(context!!, android.R.layout.simple_list_item_1, smsList )
-        //conversation_list.adapter = adapter
+        val adapter: ArrayAdapter<String?> = ArrayAdapter(context!!, android.R.layout.simple_list_item_1, listItems as MutableList<String?>)
+        conversationListView?.adapter = adapter
     }
 
     companion object {
